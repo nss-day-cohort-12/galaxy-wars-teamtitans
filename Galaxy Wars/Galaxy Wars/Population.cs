9e618fb1358a15d31c679ed1ffddbab7053e319f
Martin Cross <mncross@gmail.com>
2016-04-18 10:16:45 -0500

    add Population class

---
+++ Galaxy Wars/Galaxy Wars/Population.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars
+{
+  class Population
+  {
+    private int _saiyanPopulation = 1000000;
+    public int saiyanPopulation
+    {
+      get
+      {
+        return _saiyanPopulation;
+      }
+      set
+      {
+        _saiyanPopulation = value;
+      }
+    }
+
+    private int _wookiePopulation = 1000000;
+    public int wookiePopulation
+    {
+      get
+      {
+        return _wookiePopulation;
+      }
+      set
+      {
+        _wookiePopulation = value;
+      }
+    }
+
+    private int _ewokPopulation = 1000000;
+    public int ewokPopulation
+    {
+      get
+      {
+        return _ewokPopulation;
+      }
+      set
+      {
+        _ewokPopulation = value;
+      }
+    }
+  }
+}
