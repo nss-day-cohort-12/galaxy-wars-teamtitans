75b30740509a6ace39717b5ce652bd56b2bcd6b4
Martin Cross <mncross@gmail.com>
2016-04-17 23:46:33 -0500

    begin work on IWarrior and Wookie class

---
+++ Galaxy Wars/Galaxy Wars/Species/IWarrior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Species
+{
+  interface IWarrior
+  {
+    // attack bonus
+    Species BerserkerBonus(int battleYear);
+
+    // health bonus
+    int TacticalBonus(int battleYear);
+  }
+}
