714b3f3360be4bf9b7daf5d459249fbdb84e43c6
Jason Weakley <jason.b.weakley@gmail.com>
2016-04-19 13:26:23 -0500

    storyline

---
+++ Galaxy Wars/Galaxy Wars/Battle/IBattle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Battle
+{
+  interface IBattle
+  {
+    public Battle()
+    {
+      // I don't know what I am doing
+    }
+  }
+}
---
+++ Galaxy Wars/Galaxy Wars/Storyline/Storyline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Storyline
+{
+  public class Storyline
+  {
+    public Prologue()
+    {
+      return "The races of the Solaris System have known two stars since time immemorial. Each society has waxed and wained as the Red and Yellow Giants dance around each other, for periods lasting nearly a generation. Now at the dawn of a 27th Red Cycle war has broken out between the Saiyans, Ewoks, and Wookies for control of the System, each desiring to lay claim to the many rich and uninhabited worlds each species needs for their growing populations.";
+    }
+  }
+}
